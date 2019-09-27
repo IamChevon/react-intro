cc9883f7e08eaaecf88fe59fe5aac33bc6d018b2
Chevon Williams <cwilliams@SMS.LOCAL>
2019-09-27 18:06:47 -0500

    lol

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const firstelement = <h1>Hello from React</h1>
+import React from "react";
+import ReactDOM from "react-dom";
+// const firstelement = (
+//   <div>
+//     <h1>Hello from React</h1>
+//     <p>This is a aragraph</p>
+//   </div>
+// );
 
-ReactDOM.render(firstelement, document.getElementById('root'));
+class Car extends React.Component {
+    
+        render() {
+            return <h2> Hi, Im a Car</h2>;
+        }
+}
 
+ReactDOM.render(<Car/>, document.getElementById("root"));
+
+// ReactDOM.render(firstelement, document.getElementById("root"));

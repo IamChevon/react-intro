06b273c756cd87fb3c055eab1b52e6f06aba730e
IamChevon <w2chevon@gmail.com>
2019-09-28 15:10:19 -0500

    refactoring. implement nested components.

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Car from './Car.js';
 // const firstelement = (
 //   <div>
 //     <h1>Hello from React</h1>
@@ -7,13 +8,40 @@ import ReactDOM from "react-dom";
 //   </div>
 // );
 
-class Car extends React.Component {
-    
-        render() {
-            return <h2> Hi, Im a Car</h2>;
-        }
+// // Basic Class component with props
+// class Car extends React.Component {
+
+//     render() {
+//         return <h2>A {this.props.color} Car</h2>;
+//     }
+// }
+
+
+// class component with parent construtor and state
+
+// class Car extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {color: 'Red'}
+//     }
+
+//     render() {
+//         return <h2>I am a {this.state.color} car.</h2>
+//     }
+// }
+
+class Garage extends React.Component {
+    render() {
+        const carinfo = {name: "Ford", model: "Mustang"};
+        return (
+            <div>
+                <h1>Who lives in my Garage?</h1>
+                <Car color="red" brand={carinfo}/>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<Car/>, document.getElementById("root"));
+ReactDOM.render(<Garage />, document.getElementById("root"));
 
 // ReactDOM.render(firstelement, document.getElementById("root"));

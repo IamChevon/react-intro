5e414f3b8bd2519d167329e98f3e3e282fed4db4
IamChevon <w2chevon@gmail.com>
2019-09-29 12:57:04 -0500

    added statechange implementation

--- myfirstapp/src/Car.js
+++ myfirstapp/src/Car.js
@@ -2,11 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-// Basic Class component with props
+// Basic Class component with props, state, stateChange
 class Car extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            brand: "Ford",
+            model: "Mustang",
+            color: "red",
+            year: 1986
+        };
+    }
+
+    changeColor = () => {
+        this.setState({ color: "blue" });
+    }
 
     render() {
-        return <h2>A {this.props.color} {this.props.brand.model}</h2>;
+        return (
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color} {this.state.model} from {this.state.year}.
+                </p>
+                <button type="button" onClick={this.changeColor}>Change color</button>
+            </div>
+        );
     }
 }
 

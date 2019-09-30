e7d4162e7aa73befd3ddcaa80837e119002efe9a
Chevon Williams <cwilliams@SMS.LOCAL>
2019-09-30 12:59:06 -0500

    create list component

---
+++ todo-app/src/components/list/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Item from "../item/Item";
+
+function List() {
+  return (
+    <div>
+      <Item />
+      <Item />
+      <Item />
+    </div>
+  );
+}
+
+export default List;

a6c80e61a4f3d9d0cb758ba67eec63d30ffe9649
Chevon Williams <cwilliams@SMS.LOCAL>
2019-09-30 13:00:21 -0500

    input item to add list item.

---
+++ todo-app/src/components/input/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Input() {
+    return (
+        <div>
+            <input type="text" name="new-item"/>
+            <button>Add item</button>
+        </div>
+        
+    );
+}
+
+export default Input;

4ed0169b3497af178f2685266cd6aa9b08e0c5f9
IamChevon <w2chevon@gmail.com>
2019-10-06 21:54:09 -0500

    refactoring. passed newitem method as props from parent component.

--- todo-app/src/components/input/Input.js
+++ todo-app/src/components/input/Input.js
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Input.css";
 
-const Input = () => {
-  const [inputData, setInputData] = useState("");
+const Input = (props) => {
+  
 
   const handleUpdate = event => {
     let input;
     if (event.type === "click") {
       input = document.getElementById("new-item").value;
     }
-    setInputData(input);
-
-    console.log(inputData);
+    props.newItem(input);
   };
 
   return (
     <div id="container">
-      <p>You entered {inputData}</p>
       <input type="text" id="new-item" name="new-item" />
       <button id="add-button" onClick={handleUpdate}>
-        Add item
+        Add Item
       </button>
     </div>
   );

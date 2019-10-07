51cdce20b316325ce18f313e6f7d2b396bcebe74
IamChevon <w2chevon@gmail.com>
2019-10-06 21:52:58 -0500

    added state variable to maintain state of the list. add newItem function to update the state using the value from input field.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/header/Header";
 import List from "./components/list/List";
 import Input from "./components/input/Input";
 
+
+
 function App() {
+
+  const [ListItem, setListItem] = useState([]);
+
+  const newItem = (item) => {
+    //  theList.push(item);
+    setListItem([...ListItem, item]);
+
+    console.log("added new item");
+  };
+
+
   return (
     <div>
       <Header />
-      <List />
-      <Input />
+      <List list={ListItem} />
+      <Input newItem={newItem} />
     </div>
   );
+
 }
 
 export default App;

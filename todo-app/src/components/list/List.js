f249d014f974dfaf2f5bd87e4bc25e019f7ae0af
IamChevon <w2chevon@gmail.com>
2019-11-11 19:38:26 -0500

    refactoring. add delete item method.

--- todo-app/src/components/list/List.js
+++ todo-app/src/components/list/List.js
@@ -4,6 +4,15 @@ import "./List.css";
 
 const List = props => {
 
+
+  const deleteItem = (index) => {
+    props.deleteItem(index);
+  }
+
+  const createList = list => {
+    return list.map(item => <Item name={item} key={item} deleteItem={deleteItem} />)
+  }
+
   const todoList = createList(props.list);
 
   return (
@@ -13,8 +22,6 @@ const List = props => {
   );
 }
 
-const createList = list => {
-  return list.map(item => <Item name={item} key={item}/>)
-}
+
 
 export default List;
